2dc85212b2d910bd7fd01ea23dff7ce359f6ffdf
Vale Eseve <vale.eseve@gmail.com>
2025-12-16 20:18:29 -0300

    Fix para csrf railway

--- config/settings.py
+++ config/settings.py
@@ -153,3 +153,7 @@ MESSAGE_TAGS = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://apothecaria.railway.app',
+]
